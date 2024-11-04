ae29ec52bda8a550087f8fc5b758dcac671165b2
Avlie <antipovicana@gmail.com>
2024-11-04 09:00:36 +0100

    update

--- app/src/main/java/com/example/kosci/MainActivity.java
+++ app/src/main/java/com/example/kosci/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
     private TextView[] diceViews;
     private TextView rollResultView, totalScoreView, rollCountView;
@@ -43,17 +44,69 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 rollDice();
-        }
+            }
+        });
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 resetGame();
             }
-
-}
-        private void rollDice() {
+        });
+    }
+        private void rollDice () {
             int[] diceValues = new int[5];
-        for (int i = 0; i < 5; i++) {
-            diceValues[i] = random.nextInt(6) + 1;
+            int currentRollScore = 0;
+
+
+            for (int i = 0; i < 5; i++) {
+                diceValues[i] = random.nextInt(6) + 1;
+                diceViews[i].setText(String.valueOf(diceValues[i]));
+            }
+
+
+            currentRollScore = calculateRollScore(diceValues);
+            rollResultView.setText("Wynik tego losowania: " + currentRollScore);
+
+            updateScore(currentRollScore);
+            updateRollCount();
+        }
+        private int calculateRollScore ( int[] diceResults){
+            int rollscore = 0;
+            int[] dicecounts = new int[6];
+
+            for (int value : diceResults) {
+                dicecounts[value - 1]++;
+            }
+
+            for (int i = 0; i < dicecounts.length; i++) {
+                if (dicecounts[i] > 1) {
+                    rollscore += (i + 1) * dicecounts[i];
+                }
+            }
+            return rollscore;
+        }
+
+
+        private void updateScore ( int currentRollScore){
+            totalScore += currentRollScore;
+            totalScoreView.setText("Wynik gry: " + totalScore);
 
         }
+        private void updateRollCount () {
+            rollCount++;
+            rollCountView.setText("Liczba rzutów: " + rollCount);
+        }
+
+        private void resetGame () {
+            totalScore = 0;
+            rollCount = 0;
+
+            for (TextView diceView : diceViews) {
+                diceView.setText("?");
+            }
+
+            rollResultView.setText("Wynik tego losowania: 0");
+            totalScoreView.setText("Wynik gry: 0");
+            rollCountView.setText("Liczba rzutów: 0");
+        }
+}
